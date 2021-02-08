51a1301891a26cea5ab45c6536adcd0dd5d0ba85
unknown <gasymzadefarid@yandex.ru>
2021-02-08 20:43:28 +0300

    14 urok

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import classes from './Header.module.css'
+
 
 export const Header = () => {
-    return <header className='header'>
+    return <header className={classes.header}>
                 <img src="https://ak.picdn.net/shutterstock/videos/1020730591/thumb/7.jpg" alt=""/>
             </header>
 
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import classes from './Profile.module.css'
 
 export const Profile = () => {
-    return <div className='content'><img src="https://www.nultylighting.co.uk/wp-content/uploads/2012/10/retail-store-sport-red-light-tunnel-consultant-nulty-banner-1920x1080.jpg" alt=""/></div>
+    return <div className={classes.content}>
+        <img src="https://www.nultylighting.co.uk/wp-content/uploads/2012/10/retail-store-sport-red-light-tunnel-consultant-nulty-banner-1920x1080.jpg" alt=""/>
+    </div>
 
 }
