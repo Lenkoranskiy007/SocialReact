9ce650cd82c646edcf0210ca292ebb4e1acca90e
unknown <gasymzadefarid@yandex.ru>
2021-02-08 21:33:02 +0300

    15 urok

---
+++ src/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './MyPosts.module.css'
+
+export const MyPosts = () => {
+    return (
+        <div>
+            <div>My posts</div>
+            <textarea ></textarea>
+            <button>Add post</button>
+        </div>
+    )
+}
--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import classes from './Profile.module.css'
+import {MyPosts} from "./MyPosts/MyPosts";
+import {Post} from "./MyPosts/Post/Post";
 
 export const Profile = () => {
     return <div className={classes.content}>
         <img src="https://www.nultylighting.co.uk/wp-content/uploads/2012/10/retail-store-sport-red-light-tunnel-consultant-nulty-banner-1920x1080.jpg" alt=""/>
+    <div>
+        ava + description
+    </div>
+        <MyPosts/>
+        <Post/>
     </div>
 
 }
