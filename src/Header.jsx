8e98913ea823b43613a41f94b54a4c3a1c2c9121
unknown <gasymzadefarid@yandex.ru>
2021-02-08 20:21:01 +0300

    frist commit

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return <header className='header'>
+                <img src="https://ak.picdn.net/shutterstock/videos/1020730591/thumb/7.jpg" alt=""/>
+            </header>
+
+
+}
---
+++ src/Profile.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Profile = () => {
+    return <div className='content'><img src="https://www.nultylighting.co.uk/wp-content/uploads/2012/10/retail-store-sport-red-light-tunnel-consultant-nulty-banner-1920x1080.jpg" alt=""/></div>
+
+}
