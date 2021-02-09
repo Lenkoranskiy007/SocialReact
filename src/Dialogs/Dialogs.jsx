2738a1d74ce22cc4fbeaded9f20c1c60836d8729
unknown <gasymzadefarid@yandex.ru>
2021-02-09 18:17:19 +0300

    21 urok

--- src/Dialogs/Dialogs.jsx
+++ src/Dialogs/Dialogs.jsx
@@ -3,7 +3,19 @@ import classes from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-        Dialogs
+        <div className={classes.dialogs}>
+        <div className={classes.dialogsItems}>
+            <div className={classes.dialog}>Farid</div>
+            <div className={classes.dialog}>Sudjaat</div>
+            <div className={classes.dialog}>Rufat</div>
+            <div className={classes.dialog}>Adam</div>
+            <div className={classes.dialog}>Tamerlan</div>
+        </div>
+            <div className={classes.messages}>
+                <div className={classes.dialog}>Hi</div>
+                <div className={classes.dialog}>How are you , bro?</div>
+                <div className={classes.dialog}> It's my first project men</div>
+            </div>
+
     </div>)
 }
