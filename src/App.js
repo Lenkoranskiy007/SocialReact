596662640ecff2ef1ab782794dd9a62026500c96
unknown <gasymzadefarid@yandex.ru>
2021-02-09 10:37:00 +0300

    18 urok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import {Header} from "./Header/Header";
 import {Navbar} from "./Navbar/Navbar";
 import {Profile} from "./Profile/Profile";
+import {Dialogs} from "./Dialogs/Dialogs";
 
 function App() {
   return (
     <div className="App-wrapper">
         <Header />
         <Navbar/>
-        <Profile/>
+        <div className='app-wrapper-content'>
+            <Profile/>
+            <Dialogs/>
+        </div>
     </div>
   );
 }
--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -5,6 +5,7 @@ import {Post} from "./MyPosts/Post/Post";
 
 export const Profile = () => {
     return <div className={classes.content}>
+        <div>
         <img src="https://www.nultylighting.co.uk/wp-content/uploads/2012/10/retail-store-sport-red-light-tunnel-consultant-nulty-banner-1920x1080.jpg" alt=""/>
     <div>
         ava + description
@@ -12,6 +13,7 @@ export const Profile = () => {
         <MyPosts/>
         <Post message='How are You?' likesCount='10'/>
         <Post message="It's my first project men" likesCount='15'/>
+        </div>
     </div>
 
 }
