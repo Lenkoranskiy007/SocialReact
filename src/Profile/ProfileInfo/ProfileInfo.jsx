217e35a7591ed64b89458319cdd5e146e5cbc12e
Lenkoranskiy007 <gasymzadefarid@yandex.ru>
2021-03-21 11:54:41 +0300

    84 urok

--- src/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import classes from "../Profile.module.css";
 import preloader from '../../photo/preloader.gif'
 import ProfileStatus from "./ProfileStatus";
 import {updateStatusTC} from "../../redux/profile-reducer";
+import ProfileHookStatus from "./ProfileStatusHook";
 
 export const ProfileInfo = (props) => {
 
@@ -17,7 +18,7 @@ export const ProfileInfo = (props) => {
                 {/*<img src="https://www.nultylighting.co.uk/wp-content/uploads/2012/10/retail-store-sport-red-light-tunnel-consultant-nulty-banner-1920x1080.jpg" alt=""/>*/}
                 <div>
                     <img src={props.profile.photos.large} alt=""/>
-                    <ProfileStatus status={props.status} updateStatusTC={props.updateStatusTC}/>
+                    <ProfileHookStatus status={props.status} updateStatusTC={props.updateStatusTC}/>
                 </div>
                 </div>
         </div>
---
+++ src/Profile/ProfileInfo/ProfileStatusHook.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+const  ProfileHookStatus = (props) => {
+
+    const[editMode,setEditMode] = useState(false)
+    const[status, setStatus] = useState(props.status)
+
+   const  activateEditMode =  () => {
+        setEditMode(true)
+    }
+
+   const deactivateEditMode = () => {
+        setEditMode(false)
+       props.updateStatusTC(status)
+    }
+
+    const onStatusChange = (e) => {
+        let text = e.currentTarget.value
+        setStatus(text)
+    }
+
+        return editMode
+            ? <div><input autoFocus onChange={onStatusChange} onBlur={ deactivateEditMode} value={status} /></div>
+            : <div> <span onDoubleClick={ activateEditMode}>{props.status}</span></div>
+
+}
+
+export default ProfileHookStatus
