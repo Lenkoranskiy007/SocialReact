5ea27e4f87d0988e7c276b40d200ac2ef2c7f179
Lenkoranskiy007 <gasymzadefarid@yandex.ru>
2021-03-23 10:40:24 +0300

    91 urok

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {usersReducer} from "./users-reducer";
@@ -17,6 +17,8 @@ let reducers = combineReducers({
 })
 
 
-let store  = createStore(reducers, applyMiddleware(thunkMiddleware))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store  = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)))
+window.__store__  = store
 
 export default store
